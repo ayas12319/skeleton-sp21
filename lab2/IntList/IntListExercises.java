d33b2e632239f70e7af9dd91e76f73eac893ce38
ayas <3927412804@qq.com>
2025-06-02 10:31:42 +0800

    d1

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -51,10 +51,10 @@ public class IntListExercises {
      *  the first digit of x.
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
-        if(x <= 10) return false;
+        if(x < 10) return true;
         else {
             int lastDigit = x % 10;
-            while (x > 10) {
+            while (x >= 10) {
                 x = x / 10;
             }
             int firstDigit = x % 10;
