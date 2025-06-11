86736d562363b134065c51dd7ee0c6035c669ec0
ayas <3927412804@qq.com>
2025-06-11 13:42:36 +0800

    really

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,14 +4,14 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-//    public static int nextNumber(int n) {
-//        if(n % 2 == 0){
-//            return n / 2;
-//        }
-//        else{
-//            return 3 * n + 1;
-//        }
-//    }
+    public static int nextNumber(int n) {
+        if(n % 2 == 0){
+            return n / 2;
+        }
+        else{
+            return 3 * n + 1;
+        }
+    }
 
     public static void main(String[] args) {
         int n = 5;
